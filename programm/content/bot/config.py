97793e081b17526034bcf0216f56f4cfda61d706
Nikita <68381689+Good5263@users.noreply.github.com>
2020-11-22 17:00:15 +0400

    Update config.py

--- programm/content/bot/config.py
+++ programm/content/bot/config.py
@@ -1,8 +1,3 @@
-settings = {
-    'PREFIX': '!',
-    'TOKEN': 'NzQwMTAxNzcyNDQxNTUwODg4.XykHfA.gLoNWCHGDhRSXa0fBWHlWY9IkwM'
-}
-
 gifs = {
     'CLEAR': 'https://media.giphy.com/media/U17E0WQBnTrDtuGNrn/giphy.gif',
     'ERROR': 'https://media.giphy.com/media/TqiwHbFBaZ4ti/giphy.gif',
